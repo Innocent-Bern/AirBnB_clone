8c3db625be0297bc807540eb67508df320aa101b
Innocent_Bern <innocentbern22@gmail.com>
2023-08-29 14:47:14 +0300

    pycodestyle

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,12 +6,17 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
+    """Test class"""
 
     def test_id_is_uique(self):
+        """test id is unique"""
+
         base_1 = BaseModel()
         base_2 = BaseModel()
         self.assertNotEqual(base_1.id, base_2.id)
+
 
 if __name__ == '__main__':
     unittest.main()
